a355ac5d3e96e94729d3d186f098a785cfb3dd3c
menonraghu <velurajalakshmi@gmail.com>
2022-08-11 15:59:43 +0530

    Indentation corrected

--- spec/javascripts/glided_rose_spec.js
+++ spec/javascripts/glided_rose_spec.js
@@ -1,4 +1,3 @@
-
 //var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
@@ -14,41 +13,39 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(80)
   });
 	
-	it("Aged Brie increases in Quality", function() {
+  it("Aged Brie increases in Quality", function() {
     const gildedRose = new Shop([ new Item('Aged Brie', 2, 4)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(5)
   });
 	
-	it("Conjured decrease the quality", function() {
+  it("Conjured decrease the quality", function() {
     const gildedRose = new Shop([ new Item('Conjured Mana Cake', 3, 6)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(5)
   });
 	
-	it("Sulfuras no decrease in quality", function() {
+  it("Sulfuras no decrease in quality", function() {
     const gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(80)
   });
 	
-	it("Quality increases by 1", function() {
+  it("Quality increases by 1", function() {
     const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(21)
 	 });
 	 
-	it("Quality increases by 2 , sell_in < 11 ", function() {
-		const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 45)]);
+  it("Quality increases by 2 , sell_in < 11 ", function() {
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 45)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(47)
 	});
 	
-	it("Quality increases by 3 sell_in < 6", function() {
-		const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 45)]);
+  it("Quality increases by 3 sell_in < 6", function() {
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 45)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(48)
   });
-	
-});
-
+ });
